c37f6506b0ecc3b0067593578dfb5e102e4f34a9
VittorioDauria94 <dauriavittorio94@gmail.com>
2026-06-09 12:36:43 +0200

    fix: correct product image paths in database update script

--- database/2026_06_09_update_product_images.sql
+++ database/2026_06_09_update_product_images.sql
@@ -1,41 +1,41 @@
 USE greenery_db;
 
 UPDATE products
-SET image = 't-shirt-cotone-biologico.png'
+SET image = 'images/products/t-shirt-cotone-biologico.png'
 WHERE slug = 't-shirt-cotone-biologico';
 
 UPDATE products
-SET image = 'shopper-cotone-riciclato.png'
+SET image = 'images/products/shopper-cotone-riciclato.png'
 WHERE slug = 'shopper-cotone-riciclato';
 
 UPDATE products
-SET image = 'spazzolino-in-bamboo.png'
+SET image = 'images/products/spazzolino-in-bamboo.png'
 WHERE slug = 'spazzolino-in-bamboo';
 
 UPDATE products
-SET image = 'cotton-fioc-biodegradabili.png'
+SET image = 'images/products/cotton-fioc-biodegradabili.png'
 WHERE slug = 'cotton-fioc-biodegradabili';
 
 UPDATE products
-SET image = 'shampoo-solido-naturale.png'
+SET image = 'images/products/shampoo-solido-naturale.png'
 WHERE slug = 'shampoo-solido-naturale';
 
 UPDATE products
-SET image = 'sapone-solido-delicato.png'
+SET image = 'images/products/sapone-solido-delicato.png'
 WHERE slug = 'sapone-solido-delicato';
 
 UPDATE products
-SET image = 'ciotola-bamboo-animali.png'
+SET image = 'images/products/ciotola-bamboo-animali.png'
 WHERE slug = 'ciotola-bamboo-animali';
 
 UPDATE products
-SET image = 'sacchetti-biodegradabili-animali.png'
+SET image = 'images/products/sacchetti-biodegradabili-animali.png'
 WHERE slug = 'sacchetti-biodegradabili-animali';
 
 UPDATE products
-SET image = 'detersivo-eco-friendly.png'
+SET image = 'images/products/detersivo-eco-friendly.png'
 WHERE slug = 'detersivo-eco-friendly';
 
 UPDATE products
-SET image = 'spugna-naturale-compostabile.png'
+SET image = 'images/products/spugna-naturale-compostabile.png'
 WHERE slug = 'spugna-naturale-compostabile';
