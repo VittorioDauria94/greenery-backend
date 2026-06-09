49b5033e8c885f57edfc907b56dd1a6afc99bc0a
VittorioDauria94 <dauriavittorio94@gmail.com>
2026-06-09 12:25:32 +0200

    feat: add product images and update database seed

---
+++ database/2026_06_09_update_product_images.sql
@@ -0,0 +1,41 @@
+USE greenery_db;
+
+UPDATE products
+SET image = 't-shirt-cotone-biologico.png'
+WHERE slug = 't-shirt-cotone-biologico';
+
+UPDATE products
+SET image = 'shopper-cotone-riciclato.png'
+WHERE slug = 'shopper-cotone-riciclato';
+
+UPDATE products
+SET image = 'spazzolino-in-bamboo.png'
+WHERE slug = 'spazzolino-in-bamboo';
+
+UPDATE products
+SET image = 'cotton-fioc-biodegradabili.png'
+WHERE slug = 'cotton-fioc-biodegradabili';
+
+UPDATE products
+SET image = 'shampoo-solido-naturale.png'
+WHERE slug = 'shampoo-solido-naturale';
+
+UPDATE products
+SET image = 'sapone-solido-delicato.png'
+WHERE slug = 'sapone-solido-delicato';
+
+UPDATE products
+SET image = 'ciotola-bamboo-animali.png'
+WHERE slug = 'ciotola-bamboo-animali';
+
+UPDATE products
+SET image = 'sacchetti-biodegradabili-animali.png'
+WHERE slug = 'sacchetti-biodegradabili-animali';
+
+UPDATE products
+SET image = 'detersivo-eco-friendly.png'
+WHERE slug = 'detersivo-eco-friendly';
+
+UPDATE products
+SET image = 'spugna-naturale-compostabile.png'
+WHERE slug = 'spugna-naturale-compostabile';
